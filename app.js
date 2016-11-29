d459b0d33f6032205798c134a5bbd0895c10dcea
Alejandro Garcia <agash2013@outlook.com>
2016-11-29 14:56:26 -0800

    renamed the variable for user response so that it makes more sense

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
 
-// The questions in the guessing game must require a mix of yes/no answers, and user input must accept either y/n or yes/no responses, with either .toUpperCase() or .toLowerCase() used to validate the user input and accommodate users entering all-caps Y/N or YES/NO answers, too.
+// The questions in the guessing game must require a mix of yes/no answers, and user input must accept either y/n or yes/no questions, with either .toUpperCase() or .toLowerCase() used to validate the user input and accommodate users entering all-caps Y/N or YES/NO answers, too.
 
-var question1 = prompt('Does Alex have any cats?').toLowerCase();
+var response1 = prompt('Does Alex have any cats?').toLowerCase();
 
 // if (conditon is true) {
 //   do something;
@@ -13,9 +13,9 @@ var question1 = prompt('Does Alex have any cats?').toLowerCase();
 //   and maybe yet another thing;
 // }
 
-if (question1 === 'yes' || question1 === 'y' || question1 === 'yeah') {
+if (response1 === 'yes' || response1 === 'y' || response1 === 'yeah') {
   alert('Yes, Alex does have three cats');
-} else if (question1 === 'no' || question1 === 'n') {
+} else if (response1 === 'no' || response1 === 'n') {
   alert('Sorry, you\'re wrong, Alex has three cats');
 } else {
   alert('Try to answer with a yes/y or no/n next time.');
